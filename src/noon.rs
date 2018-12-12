dfa8135a82435f995faecf6cd73c7543fcd3f903
Nathan Osman <nathan@quickmediasolutions.com>
2018-12-11 22:01:56 -0800

    Fix math and add unit test.

--- src/noon.rs
+++ src/noon.rs
@@ -20,11 +20,22 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 
-use chrono::Utc;
+use chrono::prelude::*;
+
+const SECONDS_IN_A_DAY: f64 = 86400.;
+const UNIX_EPOCH_JULIAN_DAY: f64 = 2440587.5;
 
 /// Calculates the time at which the sun is at its highest altitude and returns
 /// the time as a Julian day.
 pub fn mean_solar_noon(longitude: f64, year: i32, month: u32, day: u32) -> f64 {
     let noon: i64 = Utc.ymd(year, month, day).and_hms(12, 0, 0).timestamp();
-    (noon / 86400) as f64 + 2440587.5
+    noon as f64 / SECONDS_IN_A_DAY + UNIX_EPOCH_JULIAN_DAY - longitude / 360.
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_prime_meridian() {
+        assert_eq!(super::mean_solar_noon(0., 1970, 1, 1), 2440588.);
+    }
 }
