d63d35bcfdc6a73a07e5057ad94b39beb094aec2
Rémi Dupré <remi@dupre.io>
2023-06-17 16:43:59 +0200

    test no-std wrappers

--- src/math.rs
+++ src/math.rs
@@ -6,6 +6,7 @@ pub(crate) const PI: f64 = {
     }
 
     #[cfg(feature = "no-std")]
+    #[allow(clippy::approx_constant)]
     {
         3.141_592_653_589_793
     }
@@ -40,27 +41,108 @@ pub(crate) fn signum(x: f64) -> f64 {
 
     #[cfg(feature = "no-std")]
     {
-        if x.is_sign_positive() {
-            1.
-        } else if x.is_sign_negative() {
-            -1.
-        } else {
+        if x.is_nan() {
             f64::NAN
+        } else if x.is_sign_positive() {
+            1.
+        } else {
+            -1.
         }
     }
 }
 
 macro_rules! use_std_or_libm {
-    ( $( $func: ident ),+ ) => {
+    ( $( $func: ident $doc: expr ),+ ) => {
         $(
-            pub(crate) fn $func(x: f64) -> f64 {
-                #[cfg(not(feature = "no-std"))]
-                { f64::$func(x) }
-                #[cfg(feature = "no-std")]
-                { libm::$func(x) }
-            }
+                #[doc = $doc]
+                pub(crate) fn $func(x: f64) -> f64 {
+                    #[cfg(not(feature = "no-std"))]
+                    { f64::$func(x) }
+                    #[cfg(feature = "no-std")]
+                    { libm::$func(x) }
+                }
         )+
     };
 }
 
-use_std_or_libm!(cos, acos, sin, asin, sqrt);
+use_std_or_libm!(
+    cos "Computes the cosine of a number (in radians).",
+    sin "Computes the sine of a number (in radians).",
+    acos "
+        Computes the arccosine of a number. Return value is in radians in
+        the range [0, pi] or NaN if the number is outside the range
+        [-1, 1].
+    ",
+    asin "
+        Computes the arcsine of a number. Return value is in radians in
+        the range [-pi/2, pi/2] or NaN if the number is outside the range
+        [-1, 1].
+    ",
+    sqrt "
+        Returns the square root of a number. Returns NaN if `self` is a
+        negative number other than `-0.0`.
+    "
+);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx::assert_relative_eq;
+
+    #[allow(clippy::approx_constant)]
+    #[test]
+    fn test_pi() {
+        assert_relative_eq!(PI, 3.141_592_653_589_793)
+    }
+
+    #[test]
+    fn test_abs() {
+        assert_relative_eq!(abs(0.), 0.);
+        assert_relative_eq!(abs(1.2), 1.2);
+        assert_relative_eq!(abs(-1.2), 1.2);
+        assert!(abs(f64::NAN).is_nan());
+    }
+
+    #[test]
+    fn test_signum() {
+        assert_relative_eq!(signum(1.2), 1.);
+        assert_relative_eq!(signum(-1.2), -1.);
+        assert_relative_eq!(signum(0.), 1.);
+        assert_relative_eq!(signum(-0.), -1.);
+        assert!(signum(f64::NAN).is_nan());
+    }
+
+    #[test]
+    fn test_cos() {
+        assert_relative_eq!(cos(PI / 3.), 0.5);
+        assert_relative_eq!(cos(2. * PI / 3.), -0.5);
+    }
+
+    #[test]
+    fn test_acos() {
+        assert_relative_eq!(acos(0.5), PI / 3.);
+        assert_relative_eq!(acos(-0.5), 2. * PI / 3.);
+        assert!(acos(2.).is_nan());
+    }
+
+    #[test]
+    fn test_sin() {
+        assert_relative_eq!(sin(PI / 3.), sqrt(3.) / 2.);
+        assert_relative_eq!(sin(2. * PI / 3.), sqrt(3.) / 2.);
+    }
+
+    #[test]
+    fn test_asin() {
+        assert_relative_eq!(asin(0.5), PI / 6.);
+        assert_relative_eq!(asin(-0.5), -PI / 6.);
+        assert!(asin(2.).is_nan());
+    }
+
+    #[test]
+    fn test_sqrt() {
+        assert_relative_eq!(sqrt(0.), 0.);
+        assert_relative_eq!(sqrt(1.), 1.);
+        assert_relative_eq!(sqrt(4.), 2.);
+        assert!(sqrt(-1.).is_nan());
+    }
+}
